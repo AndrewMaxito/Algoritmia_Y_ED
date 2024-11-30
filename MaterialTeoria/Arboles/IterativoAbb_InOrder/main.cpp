7701f9b2f654fb6bbf93ff0e9ce319c16483b86c
MaxB. <150452456+AndrewMaxito@users.noreply.github.com>
2024-11-29 20:21:34 -0500

    .

---
+++ MaterialTeoria/Arboles/IterativoAbb_InOrder/main.cpp
@@ -0,0 +1,107 @@
+
+
+/* 
+ * File:   main.cpp
+ * Author: andre
+ *
+ * Created on 29 de noviembre de 2024, 05:33 PM
+ */
+
+#include <iostream>
+
+#include "ArbolBB.h"
+#include "funcionesABB.h"
+#include "funcionesAB.h"
+#include "Pila.h"
+#include "funcionesPila.h"
+
+
+using namespace std;
+
+
+//Supopniendo que siempre se encontrara el elemento
+NodoArbol * buscaNodoElemento(NodoArbol *raiz,int elemento){
+    NodoArbol *recorrido;
+    recorrido = raiz;
+    while (recorrido) {
+        if (recorrido->elemento == elemento)return recorrido;
+        else if (recorrido->elemento > elemento) recorrido = recorrido->izquierda;
+        else recorrido = recorrido->derecha;
+    }
+    //Solo por si acaso
+    cout << "No se encontro el nodo "<<endl;
+    exit(1);
+}
+
+void recorrerEnOrden_Iterativo(ArbolBinarioBusqueda arbol){
+    if (esNodoVacio(arbol.arbolBinario.raiz)) return ;
+    Pila pilaAux;
+    construir(pilaAux);
+    
+    NodoArbol *recorrido;
+    
+    recorrido = arbol.arbolBinario.raiz;
+    
+    //
+    while (true) {
+        //Llega hasta un nodo que no tiene hijos_izqierda
+        while (recorrido) {
+            apilar(pilaAux,recorrido->elemento);
+            recorrido=recorrido->izquierda;
+        }
+        if (esPilaVacia(pilaAux)) break; //termina recorrido si ya no tiene elementos la pila
+        
+        //Si aun hay elemento en la pila (aun no termina)
+        cout << cima(pilaAux)<<"  ";
+        recorrido = buscaNodoElemento(arbol.arbolBinario.raiz,desapilar(pilaAux));
+        
+        //Va a la derecha
+        if (not esNodoVacio(recorrido->derecha)) recorrido = recorrido->derecha;
+        else recorrido = nullptr;//esta en una hoja
+    }
+}
+
+void recorrerPreOrden_Iterativo(ArbolBinarioBusqueda arbol){
+    if (esNodoVacio(arbol.arbolBinario.raiz))return ;
+    
+    Pila pilaAux;
+    construir(pilaAux);
+    NodoArbol *recorrido = arbol.arbolBinario.raiz;
+    
+    
+    apilar(pilaAux,recorrido->elemento);
+    //
+    while (not esPilaVacia(pilaAux)) {
+        //muestra actual
+        cout << cima(pilaAux) << endl;
+        recorrido = buscaNodoElemento()
+                //Por terminar 
+    }
+
+    
+    
+    
+}
+
+int main(int argc, char** argv) {
+    ArbolBinarioBusqueda arbol1;
+
+    construir(arbol1);
+    insertar(arbol1, 50);
+    insertar(arbol1, 30);
+    insertar(arbol1, 70);
+    insertar(arbol1, 10);
+    insertar(arbol1, 40);
+    insertar(arbol1, 60);
+    insertar(arbol1, 80);
+    insertar(arbol1, 5);
+    insertar(arbol1, 20);
+    insertar(arbol1, 35);
+    insertar(arbol1, 45);
+    insertar(arbol1, 6);
+    
+    recorrerEnOrden_Iterativo(arbol1);
+    
+    return 0;
+}
+
